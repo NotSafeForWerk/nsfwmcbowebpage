3b57bbd0441f9c6670b45f5600a98ff6ac55f95f
Joseph Hurtado <nsfwmcbo@gmail.com>
2024-07-04 18:36:07 -0400

    last one today

--- src/components/TeeShort.vue
+++ src/components/TeeShort.vue
@@ -5,17 +5,14 @@
 #  ▀▀ █▪ ▀█▄▀▪ ▀▀▀      ▀▀▀▀  ▀  ▀ ▀▀▀  ▀▀▀     ▀▀▀  ▀█▄▀▪.▀  ▀     ▀▀▀▀ ▀▪ ▀█▄▀▪.▀  ▀·▀  ▀
 <script setup>
 import { ref, computed } from 'vue';
-
 import TeeData from './TeeData.json';
-
 const maxItems = ref(4);
-
 const displayedTshirts = computed(() => {
   return TeeData.slice(0, maxItems.value);
 });
+
+
 </script>
-
-
 <template>
   <main>
     <div class="container p-4">
@@ -28,8 +25,7 @@ const displayedTshirts = computed(() => {
             <div class="card-footer text-center">
             </div>
           </div>
-          <a :href="`/teedetails?id=${shirt.id}`" class="btn btn-secondary stretched-link">Más detalles</a>
-   
+          <RouterLink class="btnSiplhes" :to="`/teedetails?id=${shirt.id}`">More</RouterLink>
         </div>
         <div class="mx-auto d-flex justify-content-center text-center p-2">
           <RouterLink class="btnSiplhes" to="/Tshirts">Ver mas diseños</RouterLink>
